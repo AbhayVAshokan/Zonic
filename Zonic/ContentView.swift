459115dfaf6b8a7927b475464089045f9dfe1c15
Abhay V Ashokan <avashokan@gitlab.com>
2025-11-29 16:53:07 +0530

    Initial Commit

---
+++ Zonic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zonic
+//
+//  Created by Abhay V Ashokan on 29/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
