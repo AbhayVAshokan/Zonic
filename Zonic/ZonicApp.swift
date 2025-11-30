eaacd73d2e8e667cf8b07e3bb361449b47c8ecc4
Abhay V Ashokan <avashokan@gitlab.com>
2025-11-30 19:47:07 +0530

    Add limit of 15 favorites

--- Zonic/ZonicApp.swift
+++ Zonic/ZonicApp.swift
@@ -21,10 +21,6 @@ struct ZyloqApp: App {
                             Spacer()
                             TimeField(timezone: favorite.place.timezone)
                             Menu {
-                                Button("Edit") {
-                                    // TODO: Implement edit functionality
-                                    print("Edit functionality for favorite \(favorite.id) needs to be implemented.")
-                                }
                                 Button("Delete") {
                                     removeFavorite(db: db, id: favorite.id)
                                     favorites = fetchFavorites(db: db)
@@ -41,50 +37,66 @@ struct ZyloqApp: App {
                 }
 
                 if favorites.count < 15 {
-//                    TODO: Auto-focus the text field on appear.
+                    //                    TODO: Auto-focus the text field on appear.
                     TextField("Enter your city", text: $searchText)
                         .onChange(of: searchText) { _, val in
                             places = fetchPlaces(db: db, searchText: searchText)
                         }
-                }
-
-                if searchText != "" {
-                    Divider()
-                    
-                    if places.isEmpty {
-                        Text("No places found")
-                    }
-                }
-                
-                if !places.isEmpty {
-                    ForEach(places, id: \.id) { place in
-                        let isFavorite = favorites.contains(where: { $0.place.id == place.id })
-                        
-                        Button {
-                            if !isFavorite {
-                                addFavorite(db: db, place: place)
-                                favorites = fetchFavorites(db: db)
-                            }
-                        } label: {
-                            HStack {
-                                Text(place.flag)
-                                Text(place.name)
-                                Spacer()
-                                TimeField(timezone: place.timezone)
-                            }
-                            .padding(.vertical, 2)
-                            .contentShape(Rectangle())
-                            .onHover { hovering in
-                                if hovering {
-                                    NSCursor.pointingHand.push()
-                                } else {
-                                    NSCursor.pop()
+                        .onSubmit {
+                            if !places.isEmpty {
+                                let isFavorite = favorites.contains(where: { $0.place.id == places.first!.id })
+                                if !isFavorite {
+                                    addFavorite(db: db, place: places.first!)
+                                    favorites = fetchFavorites(db: db)
                                 }
                             }
                         }
-                        .buttonStyle(.plain)
-                        .opacity(isFavorite ? 1 : 0.5)
+                    
+                    
+                    if searchText != "" {
+                        Divider()
+                        
+                        if places.isEmpty {
+                            Text("No places found")
+                        }
                     }
+                    
+                    if !places.isEmpty {
+                        ForEach(places, id: \.id) { place in
+                            let isFavorite = favorites.contains(where: { $0.place.id == place.id })
+                            
+                            Button {
+                                if !isFavorite {
+                                    addFavorite(db: db, place: place)
+                                    favorites = fetchFavorites(db: db)
+                                }
+                            } label: {
+                                HStack {
+                                    Text(place.flag)
+                                    Text(place.name)
+                                    Spacer()
+                                    TimeField(timezone: place.timezone)
+                                }
+                                .padding(.vertical, 2)
+                                .contentShape(Rectangle())
+                                .onHover { hovering in
+                                    if hovering {
+                                        NSCursor.pointingHand.push()
+                                    } else {
+                                        NSCursor.pop()
+                                    }
+                                }
+                            }
+                            .buttonStyle(.plain)
+                            .opacity(isFavorite ? 0.5 : 1)
+                        }
+                    }
+                } else {
+                    Text("Max. 15 favorites are permitted")
+                        .onAppear {
+                            searchText = ""
+                            places = []
+                        }
                 }
 
                 Divider()
