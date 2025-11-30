f00350078306bce8032e68a6db77b959b3382c3b
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-11-30 23:01:46 +0530

    Initial Commit

---
+++ Zonic/ZonicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZonicApp.swift
+//  Zonic
+//
+//  Created by Abhay V Ashokan on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ZonicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
